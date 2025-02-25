d40b194dfdd3fb4f363aa0379c1486ad6a736445
kevinbraunstein <kevin.braunstein@torontomu.ca>
2025-02-25 10:34:15 -0500

    Update worker3.js

--- worker3.js
+++ worker3.js
@@ -1,6 +1,9 @@
+// worker3.js (worker file)
+
 self.onmessage = (event) => {
-  if(event.data === "checkCSP") {
-    if (document.securityPolicy) {
+  if (event.data === "checkCSP") {
+    //check if document is define before accessing to document.securityPolicy
+    if (typeof document !== 'undefined' && document.securityPolicy) {
       postMessage({ type: 'error', message: 'worker3.js - document.securityPolicy should not be accessible in worker' });
     } else {
       postMessage({ type: 'success', message: 'worker3.js - document.securityPolicy is not accessible in worker' });
